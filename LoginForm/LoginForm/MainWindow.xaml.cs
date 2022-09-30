c269fe7a7d29c42a37b5f75f442684263a5640c8
Thanh Phong <ngt.phongg@gmail.com>
2022-09-30 10:33:49 +0700

    upload source code

---
+++ LoginForm/LoginForm/MainWindow.xaml.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace LoginForm
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            if (Properties.Settings.Default.username != String.Empty)
+            {
+                txtUsername.Text = Properties.Settings.Default.username;
+                txtPassword.Password = Properties.Settings.Default.password;
+                cbSave.IsChecked = true;
+            }
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            if (txtUsername.Text == "" || txtPassword.Password.ToString() == "")
+            {
+                MessageBox.Show("Please provide UserName and Password");
+                return;
+            }
+            if (txtUsername.Text != "admin" || txtPassword.Password.ToString() != "admin")
+            {
+                MessageBox.Show("Login Fail!");
+                return;
+            }
+            if (txtUsername.Text == "admin" && txtPassword.Password.ToString() == "admin")
+            {
+                if (cbSave.IsChecked==true)
+                {
+                    Properties.Settings.Default.username = txtUsername.Text;
+                    Properties.Settings.Default.password = txtPassword.Password.ToString();
+                    Properties.Settings.Default.Save();
+                }
+                else
+                {
+                    Properties.Settings.Default.username = null;
+                    Properties.Settings.Default.password = null;
+                    Properties.Settings.Default.Save();
+                }
+                MessageBox.Show("Login Successful");
+                return;
+            }
+        }
+    }
+}
---
+++ Security/Security/MainWindow.xaml.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Security
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void btnClick(object sender, RoutedEventArgs e)
+        {
+            Button btn = (Button)sender;
+            txtCode.Password += btn.Content;
+        }
+
+        private void btn1_Click(object sender, RoutedEventArgs e)
+        {
+            btnClick(sender, e);
+        }
+
+        private void btn9_Click(object sender, RoutedEventArgs e)
+        {
+            btnClick(sender, e);
+
+        }
+
+        private void btn2_Click(object sender, RoutedEventArgs e)
+        {
+            btnClick(sender, e);
+
+        }
+
+        private void btn3_Click(object sender, RoutedEventArgs e)
+        {
+            btnClick(sender, e);
+
+        }
+
+        private void btn4_Click(object sender, RoutedEventArgs e)
+        {
+            btnClick(sender, e);
+
+        }
+
+        private void btn5_Click(object sender, RoutedEventArgs e)
+        {
+            btnClick(sender, e);
+
+        }
+
+        private void btn6_Click(object sender, RoutedEventArgs e)
+        {
+            btnClick(sender, e);
+
+        }
+
+        private void btn7_Click(object sender, RoutedEventArgs e)
+        {
+            btnClick(sender, e);
+
+        }
+
+        private void btn8_Click(object sender, RoutedEventArgs e)
+        {
+            btnClick(sender, e);
+
+        }
+
+        private void btn0_Click(object sender, RoutedEventArgs e)
+        {
+            btnClick(sender, e);
+
+        }
+
+        private void btnC_Click(object sender, RoutedEventArgs e)
+        {
+            txtCode.Password ="";
+        }
+
+        private int ExistCode(string code)
+        {
+            int res = 0;
+            if(code == "1645" || code == "1689")
+            {
+                return 1;
+            }
+            if (code == "8345")
+            {
+                return 2;
+            }
+            if (code == "9998" || code == "1006" || code == "1008")
+            {
+                res = 3;
+            }
+            return res;
+        }
+
+        private void btnHashtag_Click(object sender, RoutedEventArgs e)
+        {
+            String code = txtCode.Password.ToString();
+            DateTime now = DateTime.Now;
+            if (ExistCode(code)==1)
+            {
+                activityLog.Items.Add($"{now}   Technicians");
+            } else if (ExistCode(code) == 2)
+            {
+                activityLog.Items.Add($"{now}  Custodians ");
+            } else if (ExistCode(code) == 3)
+            {
+                activityLog.Items.Add($"{now}  Scientist");
+            } else
+            {
+                activityLog.Items.Add($"{now}  Restricted Access ");
+            }
+            txtCode.Password = "";
+        }
+    }
+}
